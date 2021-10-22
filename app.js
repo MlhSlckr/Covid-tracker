57cc054d9ab341ab034e6767f13f16bc9d4b24b7
MlhSlckr <selcukermelih@gmail.com>
2021-10-22 21:45:03 +0300

    a

--- app.js
+++ app.js
@@ -6,25 +6,32 @@ function getData() {
     .then((res) => res.json())
     .then((data) => {
       for (let x = 0; x < Object.keys(data.Countries).length; x++) {
-        select.innerHTML += `<option class="option" value="${x}">${data.Countries[x].Country}</option>`;
+        select.innerHTML += `<option class="option"  value="${x}">${data.Countries[x].Country}</option>`;
       }
       select.addEventListener("click", (e) => {
         let ulke = e.currentTarget;
         let ulkeData = data.Countries[ulke.value];
-        /*
-        content.innerHTML = `
-        <h1 class="Countries">${ulkeData.Country}</h1>
-        <p class="Confirmed">Infected: ${ulkeData.TotalConfirmed}</p>
-        <p class="Deaths">Deaths: ${ulkeData.TotalDeaths}</p>
-        `;
-        */
-        content.innerHTML = `
-       <div class='bar' style='max-height:500px; height:${
-         ulkeData.TotalConfirmed * 0.002
-       }px;'>${ulkeData.TotalConfirmed}</div>
-        <div class='bar2' style='max-height:500px; height:${
-          ulkeData.TotalDeaths * 0.02
-        }px;'>${ulkeData.TotalDeaths}</div>
+        content.innerHTML = `<div class="bars">
+        <div class="left">
+          <div class="bar" style="max-height:500px; height:${
+            ulkeData.TotalConfirmed * 0.002
+          }px;">
+          </div>
+          <div class="value">
+            <p class="text">İnfected</p>
+            ${ulkeData.TotalConfirmed}
+          </div>
+        </div>
+        <div class="right">
+          <div class="bar2" style="max-height:200px; height:
+          ${ulkeData.TotalDeaths * 0.02}px;">
+          </div>
+          <div class="value">
+            <p class="text">Deaths</p>
+            ${ulkeData.TotalDeaths}
+          </div>
+        </div>
+      </div>
        `;
       });
     });
