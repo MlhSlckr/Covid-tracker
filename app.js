58c094d6ebe641857a1a179f74a279185d82aa11
MlhSlckr <selcukermelih@gmail.com>
2021-10-23 09:50:01 +0300

    a

--- app.js
+++ app.js
@@ -8,10 +8,36 @@ function getData() {
       for (let x = 0; x < Object.keys(data.Countries).length; x++) {
         select.innerHTML += `<option class="option"  value="${x}">${data.Countries[x].Country}</option>`;
       }
+      content.innerHTML = `
+        <h1 class='title'>Global</h1>
+        <div class="bars">
+        <div class="left">
+          <div class="bar" style="max-height:500px; height:${
+            data.Global.TotalConfirmed * 0.002
+          }px;">
+          </div>
+          <div class="value">
+            <p class="text">İnfected</p>
+            ${data.Global.TotalConfirmed}
+          </div>
+        </div>
+        <div class="right">
+          <div class="bar2" style="max-height:200px; height:
+          ${data.Global.TotalDeaths * 0.02}px;">
+          </div>
+          <div class="value">
+            <p class="text">Deaths</p>
+            ${data.Global.TotalDeaths}
+          </div>
+        </div>
+      </div>
+       `;
       select.addEventListener("click", (e) => {
         let ulke = e.currentTarget;
         let ulkeData = data.Countries[ulke.value];
-        content.innerHTML = `<div class="bars">
+        content.innerHTML = `
+        <h1 class='title'>${data.Countries[ulke.value].Country}</h1>
+        <div class="bars">
         <div class="left">
           <div class="bar" style="max-height:500px; height:${
             ulkeData.TotalConfirmed * 0.002
